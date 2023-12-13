90c90433a76295ab2e2e87e180c2523ca45f0f27
Adrien Regis <adrien.regis@gmail.com>
2023-12-13 14:34:58 +0100

    migrations and db created

---
+++ db/migrate/20231213132916_create_players.rb
@@ -0,0 +1,14 @@
+class CreatePlayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :birth_year
+      t.integer :points_record
+      t.float :point_average
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_13_131340) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_133023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.integer "score"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "birth_year"
+    t.integer "points_record"
+    t.float "point_average"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_13_131340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
 end
