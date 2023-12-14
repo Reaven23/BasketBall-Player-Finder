e6352bac3abecdd3fba9551efeeb6e24d12fad4d
Adrien Regis <adrien.regis@gmail.com>
2023-12-14 15:45:28 +0100

    logic implanted for the game, functionning game

--- app/javascript/quizz.js
+++ app/javascript/quizz.js
@@ -1,8 +1,47 @@
 const imagePlayer = document.getElementById('photo-joueur');
 const response = document.querySelector('#user-answer');
 const buttonHere = document.querySelector('.bout')
-console.log(response.id);
+const answer = document.querySelector(".answer")
 
+let currentQuestion = 0;
+let score = 0
+
+const displayQuestion = (data) => {
+  imagePlayer.src = data[currentQuestion].photo
+
+  buttonHere.addEventListener('click', () => {
+    const userAnswer = response.value.toLowerCase();
+    const correctAnswer = data[currentQuestion].first_name.toLowerCase() + data[currentQuestion].last_name.toLowerCase();
+    console.log(correctAnswer);
+    console.log(currentQuestion);
+
+    if (userAnswer === correctAnswer) {
+      answer.innerText = "Bien joué";
+      score += 10;
+    } else {
+      answer.innerText = "T'es nul! Essaie encore";
+    }
+
+
+    if (currentQuestion < data.length - 1) {
+      currentQuestion++;
+      imagePlayer.src = data[currentQuestion].photo;
+      response.value = '';
+
+      setTimeout(() => {
+        answer.innerText = '';
+      }, 1500);
+    } else {
+      console.log('Score final :', score);
+      answer.innerText = `Score final : ${score}`;
+      buttonHere.classList.add('d-none');
+      document.querySelector(".response").classList.add("d-none");
+
+    }
+
+  })
+
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,17 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data[1].first_name);
-      imagePlayer.src = data[1].photo
-
-      buttonHere.addEventListener('click', () => {
-        console.log(response.value);
-        if (response.value.toLowerCase() === data[1].first_name.toLowerCase()) {
-          console.log('well done');
-        } else {
-          console.log('loser');
-        }
-      });
-
+      displayQuestion(data);
     })
   })
