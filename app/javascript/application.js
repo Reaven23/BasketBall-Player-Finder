9cb597898816ac1a5ca2084579eae16fea37b1f6
Adrien Regis <adrien.regis@gmail.com>
2024-03-28 12:59:03 +0100

    new input

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,10 @@ import "@popperjs/core"
 import "bootstrap"
 import "./quizz"
 import "./quizz_easy"
+
+document.getElementById('player-input').addEventListener('keypress', function(event) {
+  if (event.which === 13) { // Vérifie si la touche pressée est "Entrée"
+      event.preventDefault();
+      document.getElementById('myForm').submit(); 
+  }
+});
