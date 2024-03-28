d434e5be6957aafba63b904fcd329419acd700fa
Adrien Regis <adrien.regis@gmail.com>
2024-03-28 15:28:00 +0100

    button modified

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,17 @@ import "bootstrap"
 import "./quizz"
 import "./quizz_easy"
 
+const buttonHere = document.querySelector('.bout');
+const response = document.querySelector('#player-input')
+
+document.getElementById('player-input').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') { // Vérifie si la touche pressée est "Entrée"
+      event.preventDefault(); // Empêche le comportement par défaut de la touche "Entrée" (par exemple, saut de ligne)
+      document.getElementById('check-button').click(); // Déclenche le clic sur le bouton "Vérifier"
+  }
+});
+
+buttonHere.addEventListener('click', () => {
+  const userAnswer = response.value.toLowerCase();
+  console.log(userAnswer);
+ })
