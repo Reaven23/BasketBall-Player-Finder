7b175bb0757bbbb5131a2b082ab5f3ddadc69d60
Adrien Regis <adrien.regis@gmail.com>
2024-04-02 18:42:53 +0200

    debugging js

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,7 +6,7 @@ class GamesController < ApplicationController
   def start_easy_game
     @game = Game.new
     @game.user = current_user
-    @game.name = "Game n°#{Game.all.length}"
+    @game.name = "Game n°#{Game.last.id + 1}"
     if @game.save!
       redirect_to game_path(@game)
     else
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,17 +6,3 @@ import "bootstrap"
 import "./quizz"
 import "./quizz_easy"
 
-const buttonHere = document.querySelector('.bout');
-const response = document.querySelector('#player-input')
-
-document.getElementById('player-input').addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') { 
-      event.preventDefault();
-      document.getElementById('check-button').click();
-  }
-});
-
-buttonHere.addEventListener('click', () => {
-  const userAnswer = response.value.toLowerCase();
-  console.log(userAnswer);
- })
