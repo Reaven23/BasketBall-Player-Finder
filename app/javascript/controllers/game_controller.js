87226d3fe28cc5323cdd39b26912c03a655ab6f0
Adrien Regis <adrien.regis@gmail.com>
2024-05-28 18:38:12 +0200

    game functionning

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="game"
 export default class extends Controller {
-  static targets = ["playerImage", "playerInput", "checkButton", "progressGameBar", "questionNumber", "answer", "scoreVisual"];
+  static targets = ["playerImage", "playerInput", "checkButton", "progressGameBar", "questionNumber", "answer", "scoreVisual", "score", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 
   connect() {
     console.log("hello game");
@@ -44,38 +44,45 @@ export default class extends Controller {
 
     if (userAnswer === correctAnswer) {
       this.score += 10;
+      console.log(this.score);
       this.updateScoreVisual(true);
     } else {
       this.updateScoreVisual(false);
     }
-
+    const bar = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    this[bar[this.currentQuestion] + "Target"].classList.add('orange');
     this.currentQuestion++;
     this.showNextPlayer();
   }
 
   updateScoreVisual(correct) {
+    console.log(correct);
     if (correct) {
       this.scoreVisualTarget.innerHTML = `
-        <div class="checked d-flex flex-column align-items-center justify-content-center">
-          <div class="img-container"><img src="<%= asset_path('checked.png') %>" alt="" id="image" class=""></div>
-          <div class="score-message">C'est fort, bravo!</div>
-        </div>
-      `;
-    } else {
-      this.scoreVisualTarget.innerHTML = `
-        <div class="crossed visual d-flex flex-column align-items-center justify-content-center">
-          <div class="img-container"><img src="<%= asset_path('crossed.png') %>" alt="" id="image" class=""></div>
-          <div class="score-message">C'est nul, essaie encore!</div>
-        </div>
-      `;
+      <div class="checked d-flex flex-column align-items-center justify-content-center">
+        <div class="img-container"><img src="/assets/checked.png" alt="" id="image" class=""></div>
+        <div class="score-message">C'est fort, bravo!</div>
+      </div>
+    `;
+    this.scoreTarget.innerHTML = `${this.score} point${this.score > 0 ? 's' : ''}`
+  } else {
+    console.log("hello false");
+    this.scoreVisualTarget.innerHTML = `
+      <div class="crossed visual d-flex flex-column align-items-center justify-content-center">
+        <div class="img-container"><img src="/assets/crossed.png" alt="my image" id="image" class=""></div>
+        <div class="score-message">C'est nul, essaie encore!</div>
+      </div>
+    `;
+      console.log(this.scoreVisualTarget.innerHTML);
+      this.scoreTarget.innerHTML = `${this.score} point${this.score > 0 ? 's' : ''}`
     }
+
     this.scoreVisualTarget.classList.remove('d-none');
   }
 
   endGame() {
-    alert(`Jeu terminé! Votre score est de ${this.score}`);
-    this.checkButtonTarget.classList.add('d-none');
     this.playerInputTarget.classList.add('d-none');
+    alert(`Jeu terminé! Votre score est de ${this.score} point${this.score > 0 ? 's' : ''}`);
   }
 
 
