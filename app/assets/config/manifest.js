f7aa75b827ceecd62857576ede77540284cbbfae
Adrien Regis <adrien.regis@gmail.com>
2024-06-01 12:31:37 +0200

    new home page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,8 @@
 //= link_tree ../images
+//= link_tree ../videos
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
+
