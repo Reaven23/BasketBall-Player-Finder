cd891765a8d3d19bdb7b2ba621503df8e72da1fc
Reaven23 <adrien.regis@gmail.com>
2024-10-24 17:38:22 +0200

    avatar create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :photo, :selected_avatar])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :selected_avatar
+
   has_many :games
   has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_commit :attach_selected_avatar, on: [:create, :update]
 
+  private
+
+  def attach_selected_avatar
+    p "hello je suis dans attach machin"
+    if selected_avatar.present? && !photo.attached?
+      avatar_path = Rails.root.join("app/assets/images/avatars/#{selected_avatar}.webp")
+      p avatar_path
+      photo.attach(io: File.open(avatar_path), filename: "#{selected_avatar}.webp", content_type: 'image/webp')
+    end
+  end
 end
