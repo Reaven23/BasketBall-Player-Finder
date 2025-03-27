0ae6d7b178e4fe96f6fa0e1a253a3d29ecbc4d2f
Reaven23 <adrien.regis@gmail.com>
2025-03-27 10:50:39 +0100

    cleaned page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,9 @@ class PagesController < ApplicationController
     @users_sorted_by_score = User.all.order(Arel.sql('COALESCE(points, 0) DESC'))
   end
 
+  def dashboard
+  end
+
   def contact
   end
 
